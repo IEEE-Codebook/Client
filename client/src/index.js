e2c56e7e4d08a349b30fd4cf2c1fe0c8bb217297
Radhika Chhabra <7radhikachhabra7@gmail.com>
2022-11-08 20:40:50 +0530

    google oauth

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="5030027020-n6huku4buvvicch0l62tqmqijf6rv9n4.apps.googleusercontent.com"><App /></GoogleOAuthProvider>
   </React.StrictMode>
 );
 
