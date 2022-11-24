874f90819bf4d45daa6bf7fd5ac205942cad6ae1
Dhruvil Lakhtaria <lakhtariadhruvil123@gmail.com>
2022-11-25 00:29:34 +0530

    added calendar heatmap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Calendar from './components/Calendar';
 import GoogleOAuth from "./components/GoogleOAuth";
 
 function App() {
   return (
     <div className="App">
       <GoogleOAuth/>
+      <Calendar />
     </div>
   );
 }
