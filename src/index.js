8ef7a8e9cccc199ddcad580fb27a432fbc6faa6d
Vaibhav <vaibhavagarwal693@gmail.com>
2023-02-18 19:28:32 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId="5030027020-n6huku4buvvicch0l62tqmqijf6rv9n4.apps.googleusercontent.com"><App /></GoogleOAuthProvider>
-  </React.StrictMode>
-);
+import App from "./components/App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
