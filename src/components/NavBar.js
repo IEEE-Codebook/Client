3ec0bb5018d3b199a477be3be9405ef070376c4a
Vaibhav <vaibhavagarwal693@gmail.com>
2023-03-14 21:27:42 +0530

    Navbar modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,34 +6,7 @@ const NavBar=()=>{
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     return(
-        // <nav>
-        //     <div className="cbook">
-        //       <img
-        //         src="https://cdn.xxl.thumbs.canstockphoto.com/code-book-logo-icon-design-eps-vector_csp63585084.jpg"
-        //         className="image"
-        //         height={"50px"}
-        //         alt=""
-        //       />
-        //       <div className="codebook">
-        //         <a href="#">
-        //           <h1>CODEBOOKvrgrg</h1>
-        //         </a>
-        //       </div>
-        //     </div>
-        //     <div className="navbar">
-        //       <a href="#">About</a>
-        //       <a href="#">SignUp</a>
-        //       {isLoggedIn ? (
-        //         <a href="#" onClick={() => setIsLoggedIn(false)}>
-        //           Log Out
-        //         </a>
-        //       ) : (
-        //         <a href="#" onClick={() => setIsLoggedIn(true)}>
-        //           Log In
-        //         </a>
-        //       )}
-        //     </div>
-        //   </nav>
+        
 <nav>
   <div class="logo-text">
     <a href="#"><img src="https://cdn.xxl.thumbs.canstockphoto.com/code-book-logo-icon-design-eps-vector_csp63585084.jpg" alt="Logo" /></a>
@@ -41,7 +14,16 @@ const NavBar=()=>{
   </div>
   <div class="nav-links">
     <a href="#">About</a>
-    <a href="#">Login</a>
+    {isLoggedIn ? (
+             <a href="#" onClick={() => setIsLoggedIn(false)}>
+                   Log Out
+                 </a>
+               ) : (
+                 <a href="#" onClick={() => setIsLoggedIn(true)}>
+                   Log In
+                 </a>
+               )}
+    
     <a href="#">Signup</a>
   </div>
 </nav>
