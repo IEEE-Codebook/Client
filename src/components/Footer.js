8bf1c6dec2475cfee669b990b844c5d815ce3022
Samarth Namdeo <namdeosamarth@gmail.com>
2023-04-01 11:35:48 +0530

    footer modified

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,34 +4,49 @@ import "../css/Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <div className="search-bar">
-        <input type="text" placeholder="Search" />
-        <button type="submit">Go</button>
-      </div>
-      <div className="codebook-info">
-        <img
-          src="https://cdn.xxl.thumbs.canstockphoto.com/code-book-logo-icon-design-eps-vector_csp63585084.jpg"
-          className="image"
-          height={"50px"}
-          alt=""
-        />
-        <h3 className="mt-3">CodeBook</h3>
-        <p className="description">
+      <div className="footer-column-1">
+        <div className="logo-container">
+          <img
+            className="logo"
+            src="https://cdn.xxl.thumbs.canstockphoto.com/code-book-logo-icon-design-eps-vector_csp63585084.jpg"
+            alt="CodeBook Logo"
+          />
+
+          <h1>CodeBook</h1>
+        </div>
+        <p className="info">
           CodeBook is your one-stop-shop for tracking your coding progress and
           competing with friends. It consolidates your stats from multiple
           coding platforms and provides updates on upcoming contests. Stay
           up-to-date with your and your friends' activity, compare stats, and
-          compete healthily. CodeBook simplifies the process of monitoring your
-          coding progress and competition, making it a must-have tool for any
-          coding enthusiast.
+          compete healthily.
         </p>
       </div>
-      <div className="button-group">
-        <a href="/">Home</a>
-        <br />
-        <a href="/about">About Us</a>
-        <br />
-        <a href="/contact">Contact Us</a>
+      <div className="footer-column">
+        <p className="headline-1">Your One-Stop place!</p>
+      </div>
+      <div className="footer-column-links">
+        <h2>Links</h2>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact Us</a>
+          </li>
+          <li>
+            <h1></h1>
+          </li>
+          <li>
+            <a href="https://github.com/IEEE-Codebook">GitHub repo</a>
+          </li>
+        </ul>
+      </div>
+      <div className="footer-column">
+        <p className="headline">Elevate your Coding Game with CodeBook!</p>
       </div>
     </footer>
   );
